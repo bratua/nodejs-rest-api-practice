4541e703445b985449eef6fad523cc348bac363f
Sergey Lisoviy <serlipaua@gmail.com>
2023-07-04 16:04:20 +0300

    finished configuration

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
 
+const { postsRoute } = require("./routers/postsRouter");
+
 const app = express();
 
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(morgan("tiny"));
+
+app.use("/api", postsRoute);
 
 app.listen(PORT, (err) => {
    if (err) console.error(`Error at a server: ${err}!`);
